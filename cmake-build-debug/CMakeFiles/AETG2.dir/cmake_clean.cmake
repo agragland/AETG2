d7e3d096920c6ade29553daa2887ee6fffc8fe94
Andrew Ragland <AndrewRagland@my.unt.edu>
2021-01-28 15:12:10 -0600

    committing CLion project files added testPairs.cpp - generates all pairs given # of factors and levels and outputs them to terminal

---
+++ cmake-build-debug/CMakeFiles/AETG2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AETG2.dir/testPairs.cpp.obj"
+  "AETG2.pdb"
+  "AETG2.exe"
+  "AETG2.exe.manifest"
+  "libAETG2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AETG2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
